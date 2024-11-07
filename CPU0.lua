3b1f453c4fd0591729c764539f0bc2deb68f257b
Argore <argoreyt@gmail.com>
2024-11-07 23:08:26 +0100

    CPU0.lua and gl.lua

---
+++ CPU0.lua
@@ -0,0 +1,94 @@
+-- Retro Gadgets
+
+local gl = require( "gl" )
+local vc = gdt.VideoChip0
+
+local img       = require("img.lua")
+local img_top   = require("img_top.lua")
+local img_front = require("img_front.lua")
+local img_side  = require("img_side.lua")
+
+-- helper function to copy pixeldata to a renderbuffer
+function texImg2D( _buffer, _width, _height, _data:PixelData )
+	vc:SetRenderBufferSize( _buffer, _width, _height )
+	vc:RenderOnBuffer( _buffer )
+	vc:BlitPixelData(vec2(0,0),_data)
+	vc:RenderOnScreen()
+end
+
+-- try to keep polygons to a minimum
+-- gl can only handle about 7000 quads before going under 30tps
+local verticesCube = {
+	-- sides
+	{ vec3( 0.5,-0.5,-0.5), vec3( 0.5,-0.5, 0.5), vec3( 0.5, 0.5, 0.5), vec3( 0.5, 0.5,-0.5) }, -- +x
+	{ vec3(-0.5,-0.5, 0.5), vec3(-0.5,-0.5,-0.5), vec3(-0.5, 0.5,-0.5), vec3(-0.5, 0.5, 0.5) }, -- -x
+	-- top bottom
+	{ vec3( 0.5, 0.5,-0.5), vec3( 0.5, 0.5, 0.5), vec3(-0.5, 0.5, 0.5), vec3(-0.5, 0.5,-0.5) }, -- +y
+	{ vec3( 0.5,-0.5, 0.5), vec3( 0.5,-0.5,-0.5), vec3(-0.5,-0.5,-0.5), vec3(-0.5,-0.5, 0.5) }, -- -y
+	-- front back
+	{ vec3( 0.5,-0.5, 0.5), vec3(-0.5,-0.5, 0.5), vec3(-0.5, 0.5, 0.5), vec3( 0.5, 0.5, 0.5) }, -- +z
+	{ vec3(-0.5,-0.5,-0.5), vec3( 0.5,-0.5,-0.5), vec3( 0.5, 0.5,-0.5), vec3(-0.5, 0.5,-0.5) }, -- -z
+}
+
+-- camera position and rotation
+local viewPos = vec3(0,0,3)
+local viewRot = vec3(0,0,0)
+
+-- initialize gl 
+gl.Load( gdt.VideoChip0 )
+
+gl.SetParam( "gl_SORT", true )      -- sort triangles
+gl.SetParam( "gl_CULL_FACE", true ) -- backface culling
+gl.SetFOV( 90 )
+gl.SetFarClip( 10.0 )
+gl.SetViewTransform( viewPos, viewRot )
+
+-- create vertex buffer objects
+local vbQuad = gl.CreateVertexBuffer( verticesCube )
+
+-- create texture buffer
+texImg2D( 1, img_top.width,   img_top.height,   img_top:toPixelData() )
+texImg2D( 2, img_front.width, img_front.height, img_front:toPixelData() )
+texImg2D( 3, img_side.width,  img_side.height,  img_side:toPixelData() )
+local texTop = vc.RenderBuffers[ 1 ]
+local texFront = vc.RenderBuffers[ 2 ]
+local texSides = vc.RenderBuffers[ 3 ]
+
+function update()
+	local t = gdt.CPU0.Time * 2 
+		
+	-- update view transform
+	local r = math.cos( t * 0.6 )
+	local dist = 3
+	viewPos = vec3( 0, r + 0.3, dist )
+	viewRot = vec3( r / dist, 0, 0 )
+	gl.SetViewTransform( viewPos, viewRot )
+	
+	-- start drawing	
+	gl.BeginFrame()
+	gl.Clear()
+
+	-- draw stationary cube
+	gl.SetModelTransform( vec3( 0,0,0 ), vec3( 0,0,0 ) )
+
+	gl.BindTexture( texSides )
+	gl.DrawVerticesQuad( 1, 2 ) -- draw sides
+	gl.BindTexture( texTop )
+	gl.DrawVerticesQuad( 3, 2 ) -- draw top and bottom
+	gl.BindTexture( texFront )
+	gl.DrawVerticesQuad( 5, 2 ) -- draw front and back
+
+	-- draw spinning cube
+	gl.SetModelTransform( vec3( 1,0,0 ), vec3( t,t,t ) )
+
+	gl.BindTexture( texSides )
+	gl.DrawVerticesQuad( 1, 2 ) -- draw sides
+	gl.BindTexture( texTop )
+	gl.DrawVerticesQuad( 3, 2 ) -- draw top and bottom
+	gl.BindTexture( texFront )
+	gl.DrawVerticesQuad( 5, 2 ) -- draw front and back
+
+	-- finalize frame and print debug info
+	gl.EndFrame()
+	gl.PrintDebugData()
+end
---
+++ gl.lua
@@ -0,0 +1,335 @@
+local gl = {}
+
+------------------------------------------------------------
+-- renderer state ------------------------------------------
+------------------------------------------------------------
+
+local mVC:VideoChip
+
+local mVertexBufferCounter = 0
+local mVertexBuffers = {}
+local mVertexData = {}
+local mFov = 45.0
+local mAspect = 0   -- aspect ratio
+local mNearClipDist = 0.1
+local mNearFarQ = 0 -- near/far plane clamping
+local mViewTranslation = vec3( 0, 0, 0 )
+local mViewRotation    = vec3( 0, 0, 0 )
+local mModelTranslation = vec3( 0, 0, 0 )
+local mModelRotation    = vec3( 0, 0, 0 )
+
+local mDrawTarget = -1
+local mBoundTexture = nil
+local mBoundTextureIsRenderBuffer = false
+local mBoundRenderTarget : RenderBuffer = nil
+
+local mDebugPrintOffset = 0
+
+local mDrawQuads:{{}} = {}
+local mDrawTriangles:{{}} = {}
+
+local drawV0:vec3, drawV1:vec3, drawV2:vec3, drawV3:vec3
+local drawLoopBegin:number, drawLoopEnd:number
+local drawLoopSource:{{vec3}} = {}
+
+local mParams = {
+	["RGL_SORT"] = false,
+	["RGL_CULL_FACE"] = false
+}
+
+------------------------------------------------------------
+-- Maths ---------------------------------------------------
+------------------------------------------------------------
+
+function __v3RotateZ(_v:vec3, theta)
+	if theta == 0 then return _v end
+	return vec3(
+			_v.X * math.cos(theta) - _v.Y * math.sin(theta),
+			_v.X * math.sin(theta) + _v.Y * math.cos(theta),
+			_v.Z 
+	)
+end
+
+------------------------------------------------------------
+
+function __v3RotateY(_v:vec3, theta)
+	if theta == 0 then return _v end
+	return vec3(
+			_v.X *  math.cos(theta) + _v.Z * math.sin(theta),
+			_v.Y,
+			_v.X * -math.sin(theta) + _v.Z * math.cos(theta) 
+	)
+end
+
+------------------------------------------------------------
+
+function __v3RotateX(_v:vec3, theta)
+	if theta == 0 then return _v end
+	return vec3(
+			_v.X,
+			_v.Y * math.cos(theta) - _v.Z * math.sin(theta),
+			_v.Y * math.sin(theta) + _v.Z * math.cos(theta))
+end
+
+------------------------------------------------------------
+
+function __crossProduct( a, b ) : vec3
+	return vec3(
+		a.Y * (b.Z or 0) - (a.Z or 0) * b.Y, 
+		(a.Z or 0) * b.X - a.X * (b.Z or 0), 
+		a.X * b.Y - a.Y * b.X )
+end
+
+------------------------------------------------------------
+-- Static Functions ----------------------------------------
+------------------------------------------------------------
+
+function __sortQuadFunction( _a, _b ) : boolean
+	local aCentre = _a[1].Z + _a[2].Z + _a[3].Z + _a[4].Z
+	local bCentre = _b[1].Z + _b[2].Z + _b[3].Z + _b[4].Z
+	
+	return aCentre > bCentre
+end
+
+------------------------------------------------------------
+
+function __vec3ToScreen(_v:vec3, _near, _f, _a, _q)
+	local _z = math.max(_v.Z, _near/2)
+	
+	return vec3(
+		(_v.X * _f) / _z,
+		(_v.Y * _f * _a) / _z,
+		(_v.Z * _q) - (_near * _q)
+	)
+end
+
+------------------------------------------------------------
+
+function project( _v:vec3 )
+	_v = __v3RotateX(
+			__v3RotateY(
+					__v3RotateZ(_v, mModelRotation.Z)
+					,mModelRotation.Y)
+			,mModelRotation.X)
+	_v += mModelTranslation * vec3(1,1,-1) 
+	_v += mViewTranslation
+
+	local a = __vec3ToScreen(_v, 0.1, mFov, mAspect, mNearFarQ )
+	
+	return vec3(
+		(a.X + 1) * mVC.Width / 2, 
+		(a.Y + 1) * mVC.Height / 2,
+		-a.Z)
+end
+
+------------------------------------------------------------
+-- API -----------------------------------------------------
+------------------------------------------------------------
+
+function gl.Load( _vc:VideoChip )
+	mVC = _vc
+end
+
+------------------------------------------------------------
+
+function gl.DebugPrint( ... )
+	local font = gdt.ROM.System.SpriteSheets.StandardFont
+	local str = string.format( ... )
+
+	mVC:DrawText(vec2(0,mDebugPrintOffset), font, str, color.white, color.clear )
+	mDebugPrintOffset += 8
+end
+
+------------------------------------------------------------
+
+function gl.PrintDebugData()
+  gl.DebugPrint( string.format( "mVertexBufferCounter: %i", mVertexBufferCounter ) )
+	gl.DebugPrint( string.format( "mFov: %f", mFov ) ) 
+	gl.DebugPrint( string.format( "mAspect: %f", mAspect ) )
+	gl.DebugPrint( string.format( "mNearFarQ: %f ", mNearFarQ ) )
+	
+	gl.DebugPrint( "mViewTranslation" )
+	gl.DebugPrint( string.format( "  X: %f", mViewTranslation.X ) )
+	gl.DebugPrint( string.format( "  Y: %f", mViewTranslation.Y ) )
+	gl.DebugPrint( string.format( "  Z: %f", mViewTranslation.Z ) )
+	
+	gl.DebugPrint( "mViewRotation" )
+	gl.DebugPrint( string.format( "  X: %f Radians", mViewRotation.X ) )
+	gl.DebugPrint( string.format( "  Y: %f Radians", mViewRotation.Y ) )
+	gl.DebugPrint( string.format( "  Z: %f Radians", mViewRotation.Z ) )
+
+	gl.DebugPrint( string.format( "mDrawTarget: %i", mDrawTarget ) )
+	gl.DebugPrint( string.format( "mBoundTexture: %s", mBoundTexture == nil and "none" or mBoundTexture.Type ) )
+	gl.DebugPrint( string.format( "mBoundTextureIsRenderBuffer: %s", mBoundTextureIsRenderBuffer and "true" or "false" ) )
+end
+
+------------------------------------------------------------
+
+function gl.SetParam( _param:string, _value )
+	if mParams[ _param ] == nil then
+		return
+	end
+	
+	mParams[ _param ] = _value
+end
+
+------------------------------------------------------------
+
+function gl.GetParam( _param:string )
+	return mParams[ _param ]
+end
+
+------------------------------------------------------------
+
+function gl.BeginFrame()
+	mDebugPrintOffset = 0
+	table.clear( mDrawQuads )
+end
+
+------------------------------------------------------------
+
+function gl.EndFrame()
+	gl.__DrawQuads()
+end
+
+------------------------------------------------------------
+
+function gl.Clear()
+	mVC:Clear( color.black )
+end
+
+------------------------------------------------------------
+
+function gl.SetFOV( _degrees:number )
+	mFov = 1 / math.tan(math.rad( _degrees ) / 2)
+end
+
+------------------------------------------------------------
+
+function gl.SetFarClip( _far:number )
+	mAspect   = mVC.Width / mVC.Height
+	mNearFarQ = (mNearClipDist / (mNearClipDist - _far))
+end
+
+------------------------------------------------------------
+
+function gl.SetViewTransform( _translation:vec3, _rotation:vec3 )
+	mViewTranslation = _translation
+	mViewRotation = _rotation
+end
+
+------------------------------------------------------------
+
+function gl.SetModelTransform( _translation:vec3, _rotation:vec3 )
+	mModelTranslation = _translation
+	mModelRotation = _rotation
+end
+
+------------------------------------------------------------
+
+function gl.CreateVertexBuffer( _data:{{vec3}} ) : {base:number, count:number}
+	mVertexBufferCounter += 1
+	mVertexBuffers[ mVertexBufferCounter ] = { base=#mVertexData+1, count=#_data }
+	
+	for i=1, #_data do
+		mVertexData[ #mVertexData + 1 ] = _data[i]
+	end
+	
+	return mVertexBuffers[ mVertexBufferCounter ]
+end
+
+------------------------------------------------------------
+
+function gl.BindTexture( _texture )
+	mBoundTexture = _texture
+	mBoundTextureIsRenderBuffer = _texture.Type == "RenderBuffer"
+end
+
+------------------------------------------------------------
+
+function gl.BindRenderBuffer( _buffer:number )
+	if _buffer == 0 then
+		mVC:RenderOnScreen()
+	else
+		mVC:RenderOnBuffer( _buffer )
+	end
+end
+
+------------------------------------------------------------
+
+function gl.DrawVerticesTri( _base:number, _count:number )
+	local cullFace = mParams[ "RGL_CULL_FACE" ]
+	
+	for i=_base, _base + _count - 1 do
+		drawV0 = project( mVertexData[i][1] )
+		drawV1 = project( mVertexData[i][2] )
+		drawV2 = project( mVertexData[i][3] )
+		
+		if cullFace and __crossProduct( drawV1 - drawV0, drawV2 - drawV0 ).Z > 0 then
+				continue
+		end
+		
+		mVC:DrawTriangle( 
+				drawV0, 
+				drawV1,
+				drawV2,
+				color.red
+				)
+	end
+end
+
+------------------------------------------------------------
+
+function gl.DrawVerticesQuad( _base:number, _count:number )
+	for i=_base, _base + _count - 1 do
+		drawV0 = project( mVertexData[i][1] )
+		drawV1 = project( mVertexData[i][2] )
+		drawV2 = project( mVertexData[i][3] )
+		drawV3 = project( mVertexData[i][4] )
+		
+		mDrawQuads[ #mDrawQuads + 1 ] = {drawV0, drawV1, drawV2, drawV3, mBoundTexture}
+	end
+end
+
+function gl.__DrawQuads()
+	local sort = mParams[ "RGL_SORT" ]
+	local cullFace = mParams[ "RGL_CULL_FACE" ]
+	
+	if sort then
+		table.sort( mDrawQuads, __sortQuadFunction )
+	end
+	
+	for i=1,#mDrawQuads do
+		drawV0 = mDrawQuads[i][1]
+		drawV1 = mDrawQuads[i][2]
+		drawV2 = mDrawQuads[i][3]
+		drawV3 = mDrawQuads[i][4]
+		gl.BindTexture( mDrawQuads[i][5] )
+
+		if drawV0.Z < 0 and drawV1.Z < 0 and drawV2.Z < 0 and drawV3.Z < 0 then
+			continue
+		end
+		
+		if cullFace and __crossProduct( drawV1 - drawV0, drawV3 - drawV0 ).Z < 0 then
+			continue
+		end
+		
+		if mBoundTextureIsRenderBuffer then
+			mVC:RasterRenderBuffer( drawV0, drawV1, drawV2, drawV3, mBoundTexture )
+		else
+			mVC:RasterSprite( drawV0, drawV1, drawV2, drawV3, mBoundTexture, 0, 0, color.white, color.clear )
+		end
+	end
+end
+
+------------------------------------------------------------
+
+function gl.MultiDrawSprite( calls:{{}} ) 
+  for i=1, #calls do
+    mVC:DrawSprite( calls[i][1], calls[i][2], calls[i][3], calls[i][4], calls[i][5], calls[i][6] )
+  end
+end
+
+
+-- :>
+return gl
